adc01bb591029d804c8c391dbdaf31d83d44e05f
Vlad Zayats <wallhalu@gmail.com>
2024-02-24 12:32:50 +0300

    Make color rows

--- index.js
+++ index.js
@@ -13,23 +13,64 @@ const state = {
         "red",
         "blue",
         "yellow",
-        "pink"
+        "pink",
+        "green",
+        "purple",
+        "orange",
+        "white",
+        "grey",
+        "brown",
+        "beige",
+        "turquoise"
     ],
     activeColorElement: null
 };
 
-window.onload = function() {
+window.onload = function () {
     makeColorMenu();
 }
 
 function makeColorMenu() {
-    state.defaultColors.forEach(color => {
-        const colorElement = document.createElement("div");
-        colorElement.classList.add("color");
-        colorElement.style.backgroundColor = color;
-        colorElement.addEventListener("click", colorClickHandler);
-        document.querySelector(".color-menu").appendChild(colorElement);
-    });
+    let rowAmount = Math.ceil((state.defaultColors.length) / 7);
+
+    for (let i = 0; i < rowAmount; i++) {
+        makeColorRow();
+    }
+
+    const colorRows = document.querySelectorAll(".color-row");
+    console.log(colorRows);
+    for (let colorIndex = 0, row = 0; colorIndex < state.defaultColors.length; colorIndex++) {
+        if (colorIndex % 6 === 0 && colorIndex !== 0 && row !== rowAmount) {
+            row++;
+        }
+        makeColorElement(colorIndex, row);
+    }
+
+    const addColorButton = document.createElement("div");
+    addColorButton.classList.add("color");
+    addColorButton.innerText = "+";
+    addColorButton.style.textAlign = "center";
+    if (state.defaultColors.length % 7 === 0) {
+        makeColorRow();
+        rowAmount++;
+    }
+    colorRows[colorRows.length - 1].appendChild(addColorButton);
+}
+
+function makeColorRow() {
+    const colorRow = document.createElement("div");
+    colorRow.classList.add("color-row");
+    colorRow.style.display = "flex";
+    document.querySelector(".color-menu").appendChild(colorRow);
+}
+
+function makeColorElement(colorIndex, row) {
+    const colorElement = document.createElement("div");
+    colorElement.classList.add("color");
+    colorElement.style.backgroundColor = state.defaultColors[colorIndex];
+    colorElement.addEventListener("click", colorClickHandler);
+    console.log(document.querySelectorAll(".color-row"));
+    document.querySelectorAll(".color-row")[row].appendChild(colorElement);
 }
 
 function newFileClickHandler() {
